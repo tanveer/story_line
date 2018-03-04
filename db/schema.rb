5162062669f3e830ce0d552ab178812f9e4d77a3
Tanveer Bashir <tan.m.bashir@gmail.com>
2018-03-04 09:32:15 -0500

    Remove User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180302235844) do
+ActiveRecord::Schema.define(version: 20180304035816) do
 
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "username"
-    t.string "email"
+  create_table "stories", force: :cascade do |t|
+    t.string "title"
+    t.text "story"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
-
 end
